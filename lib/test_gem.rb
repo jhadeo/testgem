6b670b678edf8e91bf83fe22653f8ce46e66751e
jhadeo <jhade.orbigoso@gmail.com>
2022-06-11 13:08:29 +0800

    0.1.0 release

--- lib/test_gem.rb
+++ lib/test_gem.rb
@@ -5,4 +5,18 @@ require_relative "test_gem/version"
 module TestGem
   class Error < StandardError; end
   # Your code goes here...
+
+  class User
+    def client
+      client_name = ''
+      print "Enter the client's name: "
+      client_name = gets.chomp.strip
+      puts "Hello #{client_name}, it's time to get your hair done" 
+      sleep 3
+      puts "Drying your hair.."
+      sleep 2
+      puts "Done! please come again."
+    end
+  end
+
 end
